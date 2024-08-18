02f0552f868ba050d1407d70b3472d115968b790
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-08-18 12:50:38 +0700

    feat: add boilerplate

---
+++ src/main/java/com/project_management_springboot/ProjectManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.project_management_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project_management_springboot/ProjectManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_management_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
